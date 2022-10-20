826cca9b32bea4d7a761735bed370c96b5e30329
João Henrique Maciel <jhsmaciel15@gmail.com>
2022-10-20 09:05:47 -0300

    Ajustes nos testes

--- test/mybank_web_api/core_test.clj
+++ test/mybank_web_api/core_test.clj
@@ -1,14 +1,14 @@
 (ns mybank-web-api.core-test
   (:require
    [clojure.test :refer [deftest is testing]]
-   [mybank-web-api.core :as m.core]
-   [mybank-web-api.server :refer [server reset-server]]
-   [mybank-web-api.utils :as m.utils]
-   [mybank-web-api.db :as m.db]))
+   [mybank-web-api.components :as m.components]
+   [mybank-web-api.db :as m.db]
+   [mybank-web-api.server :refer [server]]
+   [mybank-web-api.utils :as m.utils]))
 
 (def balance-account-1-payload
   {:status 200,
-   :body "{:saldo 100}",
+   :body "{\"saldo\":100}",
    :headers
    {"Strict-Transport-Security" "max-age=31536000; includeSubdomains",
     "X-Frame-Options" "DENY",
@@ -18,7 +18,7 @@
     "X-Permitted-Cross-Domain-Policies" "none",
     "Content-Security-Policy"
     "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;",
-    "Content-Type" "text/plain"}})
+    "Content-Type" "application/json"}})
 
 (def account-not-found-payload
   {:status 400,
@@ -49,20 +49,20 @@
     "Content-Type" "application/edn"}})
 
 (deftest api-test
-  (let [_ (reset-server)
+  (let [_ (m.components/main)
         __ (m.db/reset-db)]
     (testing "Verify balances"
       (is (= balance-account-1-payload (m.utils/test-request server :get "/saldo/1")))
       (is (= account-not-found-payload (m.utils/test-request server :get "/saldo/6"))))
     (testing "Make deposits"
-      (is (= (assoc balance-account-1-payload :body "{:saldo 300.0}")
+      (is (= (assoc balance-account-1-payload :body "{\"saldo\": 300.0}")
              (m.utils/test-post server :post "/deposito/1" "200")))
-      (is (= account-not-found-payload (m.utils/test-post server :post "/deposito/5" "200"))))
+      (is (= account-not-found-payload (m.utils/test-post server :post "/deposito/5" "{\"saldo\": 200}"))))
     (testing "Make withdraw"
-      (is (= (assoc balance-account-1-payload :body "{:saldo 200.0}")
-             (m.utils/test-post server :post "/saque/1" "100")))
+      (is (= (assoc balance-account-1-payload :body "{\"saldo\": 200.0}")
+             (m.utils/test-post server :post "/saque/1" "{\"value\": 100}")))
       (is (= account-not-found-payload
-             (m.utils/test-post server :post "/saque/5" "200"))))
+             (m.utils/test-post server :post "/saque/5" "{\"value\": 300.0}"))))
     (testing "Make withdraw without balance"
       (is (= account-without-balance-payload
-             (m.utils/test-post server :post "/saque/1" "200.1"))))))
+             (m.utils/test-post server :post "/saque/1" "{\"value\": 200.1}"))))))
